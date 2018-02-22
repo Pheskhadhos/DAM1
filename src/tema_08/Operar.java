1a84c02b57528a41fd1da95dd79698741ff2098b
SAMJOR <SAMJOR@172.16.140.255>
2018-02-22 19:28:58 +0000

    Añadimos un system out print

---
+++ src/tema_08/Operar.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema_08;
+
+/**
+ *
+ * @author SAMJOR
+ */
+public class Operar {
+  
+  
+  int suma(int operando1, int operando2){
+   int suma = operando1+operando2; 
+   return suma; 
+  }
+  
+  int multiplicar (int operando1, int operando2){
+    int multiplica = operando1*operando2;
+    return multiplica;
+  }
+}
